41d74271a96020220c1b110d8d1de592f49b0868
FCJYCT <radiata03@gmail.com>
2022-02-15 22:22:09 +0900

    DummyData creator Done

--- src/test/java/com/yapp/ios2/init/DefaultDataCreator.java
+++ src/test/java/com/yapp/ios2/init/DefaultDataCreator.java
@@ -3,8 +3,12 @@ package com.yapp.ios2.init;
 import com.yapp.ios2.config.init.properties.CoverProps;
 import com.yapp.ios2.config.init.properties.FilmProps;
 import com.yapp.ios2.config.init.properties.LayoutProps;
+import com.yapp.ios2.repository.AlbumCoverRepository;
+import com.yapp.ios2.repository.AlbumLayoutRepository;
 import com.yapp.ios2.repository.FilmTypeRepository;
 import com.yapp.ios2.testConfig.TestInit;
+import com.yapp.ios2.vo.AlbumCover;
+import com.yapp.ios2.vo.AlbumLayout;
 import com.yapp.ios2.vo.FilmType;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -15,6 +19,11 @@ public class DefaultDataCreator extends TestInit {
 
     @Autowired
     FilmTypeRepository filmTypeRepository;
+    @Autowired
+    AlbumLayoutRepository albumLayoutRepository;
+    @Autowired
+    AlbumCoverRepository albumCoverRepository;
+
 
     @Autowired
     FilmProps filmProps;
@@ -46,11 +55,29 @@ public class DefaultDataCreator extends TestInit {
 
     @Test
     public void createAlbumCover() throws Exception{
-
+        for( AlbumCover albumCoverProp : coverProps.getCovers()){
+            AlbumCover albumCover = albumCoverRepository.findAlbumCoverByCode(albumCoverProp.getCode()).orElse(
+                    AlbumCover.builder()
+                            .code(albumCoverProp.getCode())
+                            .name(albumCoverProp.getName())
+                            .build()
+            );
+            albumCoverRepository.save(albumCover);
+        }
     }
 
     @Test
     public void createAlbumLayout() throws Exception{
-
+        for(AlbumLayout albumLayoutProp : layoutProps.getLayouts()){
+            AlbumLayout albumLayout = albumLayoutRepository.findAlbumLayoutByCode(albumLayoutProp.getCode()).orElse(
+                    AlbumLayout.builder()
+                            .code(albumLayoutProp.getCode())
+                            .name(albumLayoutProp.getName())
+                            .totPaper(albumLayoutProp.getTotPaper())
+                            .photoPerPaper(albumLayoutProp.getPhotoPerPaper())
+                            .build()
+            );
+            albumLayoutRepository.save(albumLayout);
+        }
     }
 }
